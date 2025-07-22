c80468df26a4256e21ee6ea96506d4e75b768ea1
dipanshupatidar77 <dipanshupatidar77@gmail.com>
2025-07-22 18:39:21 +0530

    Added the  other models

---
+++ backend/models/Booking.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    chef: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Chef',
+      required: true
+    },
+    userName: { type: String, required: true },
+    userEmail: { type: String, required: true },
+    userPhone: { type: String, required: true },
+    contactMethod: {
+      type: String,
+      enum: ['email', 'phone', 'whatsapp'],
+      required: true
+    },
+    city: { type: String, required: true },
+    location: { type: String, required: true },
+    eventDetails: { type: String },
+    numberOfGuests: { type: Number, required: true },
+    eventDate: { type: String, required: true },
+    eventTime: { type: String, required: true },
+    selectedDishes: [{ type: String }],
+    status: {
+      type: String,
+      enum: ['pending', 'accepted', 'rejected','cancelled','completed'],
+      default: 'pending'
+    },
+    cancelReason: {
+  type: String,
+  enum: ['user_cancelled', 'chef_cancelled', 'chef_blocked'], //  Optional but helpful
+},
+    paymentStatus: {
+      type: String,
+      enum: ['pending', 'done'],
+      default: 'pending'
+    },
+    transactionId: { type: String },
+paymentDate: { type: Date },
+
+    cancelledBy: {
+  type: String,
+  enum: ['user', 'chef', null],
+  default: null
+}
+,
+    review: {
+    rating: { type: Number, min: 1, max: 5 },
+    comment: { type: String }
+    },
+    //  Added for Entry OTP
+    entryOtp: { type: String },
+    entryConfirmed: { type: Boolean, default: false },
+
+    exitOtp: { type: String },
+    exitConfirmed: { type: Boolean, default: false },
+
+  },
+  { timestamps: true } //  this is the second argument to schema, keep it separate
+);
+
+module.exports = mongoose.model('Booking', bookingSchema);
---
+++ backend/models/Feedback.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const feedbackSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'accepted', 'rejected'],
+    default: 'pending',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Feedback', feedbackSchema);
